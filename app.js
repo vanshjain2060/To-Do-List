343e4e01c8fc2360762870b720fe3b53c2c59c4d
vanshjain2060 <vanshjain0311@gmail.com>
2024-05-07 22:22:12 +0530

    toDoList

--- app.js
+++ app.js
@@ -4,19 +4,32 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+app.use(express.static("public"));                                                                                                                                                           
+let items = [];
 
 app.get("/" , function(req, res) {
-    var today = new Date();
-    var currday = today.getDay();
-    var daysarr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    
-    var day = daysarr[currday];
+    let today = new Date();
+    let option = {
+        weekday : "long",
+        day : "numeric" ,
+        month : "long"
+    };
 
-    res.render("list", {kindofDay : day});
+    let day = today.toLocaleDateString("en-US", option);
+
+    res.render("list", {kindofDay : day , newListItems : items});
     
 });
 
+app.post("/" , function(req, res) {
+    let item = req.body.newItem; 
+    items.push(item);
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000.");
+    res.redirect("/");
+});
+
+
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000.");
 });
