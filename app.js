3d6fe3af5aaf5c62bd62d3a2c207b6c9552bae96
vanshjain2060 <vanshjain0311@gmail.com>
2024-05-20 03:13:11 +0530

    Add .gitignore file

--- app.js
+++ app.js
@@ -1,43 +1,133 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));                                                                                                                                                           
 
-const items = [];
-const workItems = [];
 
-app.get("/" , function(req, res) {
-    let day = date.getDay();
-    res.render("list", {listTitle : day , newListItems : items});
-    
-});
+mongoose.connect("mongodb+srv://admin-vansh:vanshjain2060@cluster0.ki3p5of.mongodb.net/todolistDB" , {useNewUrlParser : true});
 
-app.post("/" , function(req, res) {
-    let item = req.body.newItem; 
+const itemSchema = {
+    name : String
+}
 
-    if(req.body.list == "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }else {
-        items.push(item);       
-        res.redirect("/");
+const Item = mongoose.model("Item" , itemSchema);
+
+// default items which are going to be present in the data base
+const item1 = new Item({
+    name : "test1"
+})
+const item2 = new Item({
+    name : "test2"
+})
+const item3 = new Item({
+    name : "test3"
+})
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name : String,
+    items : [itemSchema]
+}
+const List = mongoose.model("List", listSchema);
+
+app.get("/", async function(req, res) {
+    try {
+      const foundItems = await Item.find({});
+      if (foundItems.length === 0) {
+        await Item.insertMany(defaultItems);
+        console.log("Default items data inserted successfully into the database.");
+      }
+      res.render("list", { listTitle: "Today", newListItems: foundItems });
+    } catch (err) {
+      console.error(err);
     }
-});
+  });
+  
 
+  app.post("/", async function(req, res) {
+    const itemName = req.body.newItem;
+    const listName = req.body.list;
+  
+    const item = new Item({
+      name: itemName
+    });
+  
+    try {
+      if (listName === "Today") {
+        await item.save();
+        res.redirect("/");
+      } else {
+        const foundList = await List.findOne({ name: listName });
+        if (foundList) {
+          foundList.items.push(item);
+          await foundList.save();
+          res.redirect("/" + listName);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+  
+
+  app.post("/delete", async function(req, res) {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.listName;
+  
+    try {
+      if (listName === "Today") {
+        await Item.findByIdAndDelete(checkedItemId);
+        console.log("Successfully deleted the item!");
+        res.redirect("/");
+      } else {
+        await List.findOneAndUpdate(
+          { name: listName },
+          { $pull: { items: { _id: checkedItemId } } }
+        );
+        res.redirect("/" + listName);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+  
+  
 
 app.get("/about" , function(req, res) {
     res.render("about");
 });
 
 
-app.get("/work" , function(req, res) {
-    res.render("list", {listTitle : "Work List" , newListItems : workItems})
-});
+// this is the dynamic route which make diffenet Lists using params
+
+app.get("/:topic", async function(req, res) {
+    const newList = _.capitalize(req.params.topic);
+  
+    try {
+      const foundList = await List.findOne({ name: newList });
+  
+      if (!foundList) {
+        const list = new List({
+          name: newList,
+          items: defaultItems
+        });
+        await list.save();
+        res.redirect("/" + newList);
+      } else {
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+  
+
 app.post("/work" , function(req, res) {
     let item = res.body.newItem;
     workItems.push(item);
@@ -47,3 +137,5 @@ app.post("/work" , function(req, res) {
 app.listen(8000, () => {
     console.log("Server is running on port 8000.");
 });
+
+
